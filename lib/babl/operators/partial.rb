getbannerman/babl

commit 173c7b556313d0d7a66dd91f6e28b0b77d0947b9

Make partial a terminal operator (#75)
 Make partial a terminal operator (simplification)

lib/babl/operators/partial.rb
@@ -8,40 +8,13 @@ module Babl
                 # Load a partial template given its name
                 # A 'lookup_context' must be defined
                 def partial(partial_name)
-                    raise Errors::InvalidTemplate, 'Cannot use partial without lookup context' unless lookup_context
-
-                    path, source, partial_lookup_context = lookup_context.find(partial_name)
-                    raise Errors::InvalidTemplate, "Cannot find partial '#{partial_name}'" unless path
-
-                    with_lookup_context(partial_lookup_context)
-                        .source(source, path, 0)
-                        .with_lookup_context(lookup_context)
-                end
-
-                def with_lookup_context(lookup_context)
-                    self.class.new(builder.dup.tap { |inst| inst.instance_variable_set(:@lookup_context, lookup_context) })
-                end
-
-                def lookup_context
-                    builder.instance_variable_get(:@lookup_context)
-                end
-            end
-
-            class AbsoluteLookupContext
-                attr_reader :search_path
-
-                def initialize(search_path)
-                    @search_path = search_path
-                    raise Errors::InvalidTemplate, 'Invalid search path' unless search_path
-                end
-
-                def find(partial_name)
-                    query = File.join(search_path, "{#{partial_name}}{.babl,}")
-                    path = Dir[query].first
-                    return unless path
-
-                    source = File.read(path)
-                    [path, source, self]
+                    construct_terminal { |ctx|
+                        lookup_context = ctx[:lookup_context]
+                        raise Errors::InvalidTemplate, 'Cannot use partial without lookup context' unless lookup_context
+                        template, new_lookup_context = lookup_context.find(partial_name)
+                        raise Errors::InvalidTemplate, "Cannot find partial '#{partial_name}'" unless template
+                        template.precompile(Nodes::TerminalValue.instance, lookup_context: new_lookup_context)
+                    }
                 end
             end
         end

spec/operators/partial_spec.rb
@@ -4,24 +4,26 @@ require 'spec_helper'
 describe Babl::Operators::Partial do
     extend SpecHelper::OperatorTesting
 
-    let(:custom_lookup_context) {
+    let(:lookup_context) {
         TestLookupContext.new(
             blabla: TestLookupContext.new(
-                "[partial('navi').partial('miche'), partial('muche')]",
-
-                miche: TestLookupContext.new(
-                    "partial('blabla')",
-
-                    blabla: TestLookupContext.new('call { 1 + self }')
-                ),
-
+                "[partial('navi'), partial('muche')]",
                 muche: TestLookupContext.new('23'),
-                navi: TestLookupContext.new(':some_property')
+                navi: TestLookupContext.new(
+                    "partial('navi')",
+                    navi: TestLookupContext.new(
+                        "partial('miche')",
+                        miche: TestLookupContext.new(
+                            "partial('blabla')",
+                            blabla: TestLookupContext.new('call { 1 + self }')
+                        )
+                    )
+                )
             )
         )
     }
-    let(:dsl) { Babl::Template.new.with_lookup_context(custom_lookup_context) }
-    let(:object) { { some_property: 12 } }
+
+    let(:object) { 12 }
 
     context 'missing partial' do
         template { partial('i_do_not_exist') }

lib/babl.rb
@@ -7,31 +7,51 @@ require 'babl/operators'
 
 module Babl
     class Config
-        attr_accessor :search_path, :preloader, :pretty, :cache_templates
+        attr_accessor :preloader, :pretty, :cache_templates, :lookup_context
 
         def initialize
-            @search_path = nil
             @preloader = Rendering::NoopPreloader
             @pretty = true
             @cache_templates = false
+            @lookup_context = nil
+        end
+    end
+
+    class AbsoluteLookupContext
+        attr_reader :search_path
+
+        def initialize(search_path)
+            @search_path = search_path
+            raise Errors::InvalidTemplate, 'Missing search path' unless search_path
+        end
+
+        def find(partial_name)
+            query = File.join(search_path, "{#{partial_name}}{.babl,}")
+            path = Dir[query].first
+            return unless path
+            source = File.read(path)
+            [Babl.source(source, path, 0), self]
         end
     end
 
     class << self
-        def compile(&block)
-            source(&block).compile(
+        def compile(*args, &block)
+            raise ArgumentError, 'Wrong number of arguments' if args.size > 1
+            raise ArgumentError, 'Template or block expected' unless args.empty? ^ block.nil?
+
+            (args.empty? ? source(&block) : template.call(args.first)).compile(
                 pretty: config.pretty,
-                preloader: config.preloader
+                preloader: config.preloader,
+                lookup_context: config.lookup_context
             )
         end
 
-        def source(&block)
-            template = Template.new
-            if config.search_path
-                ctx = Operators::Partial::AbsoluteLookupContext.new(config.search_path)
-                template = template.with_lookup_context(ctx)
-            end
-            template.source(&block)
+        def template
+            Template.new
+        end
+
+        def source(*args, &block)
+            template.source(*args, &block)
         end
 
         def configure

lib/babl/builder/chain_builder.rb
@@ -57,11 +57,7 @@ module Babl
             end
 
             def wrap
-                rescope { |bound| yield bind(bound) }
-            end
-
-            def rescope(&block)
-                dup.tap { |tb| tb.instance_variable_set(:@scope, block) }
+                self.class.new { |bound| yield bind(bound) }
             end
         end
 

lib/babl/builder/template_base.rb
@@ -15,10 +15,10 @@ module Babl
                 super(builder)
             end
 
-            def compile(preloader: Rendering::NoopPreloader, pretty: true, optimize: true)
+            def compile(preloader: Rendering::NoopPreloader, pretty: true, optimize: true, lookup_context: nil)
                 # Compute dependencies & schema on the non-simplified node tree in order
                 # to catch all errors.
-                tree = precompile
+                tree = precompile(lookup_context: lookup_context)
                 dependencies = tree.dependencies
                 schema = tree.schema
 
@@ -40,13 +40,13 @@ module Babl
             end
 
             def unscoped
-                self.class.new builder.rescope(&:itself)
+                self.class.new
             end
 
             protected
 
-            def precompile
-                builder.precompile(Nodes::TerminalValue.instance)
+            def precompile(node = Nodes::TerminalValue.instance, **context)
+                builder.precompile(node, **context)
             end
 
             def construct_node(**new_context, &block)

lib/babl/operators/parent.rb
@@ -13,7 +13,7 @@ module Babl
                 protected
 
                 # Override TemplateBase#precompile to add parent dependencies verification
-                def precompile
+                def precompile(*)
                     Nodes::Parent::Verifier.new(super)
                 end
             end

lib/babl/operators/pin.rb
@@ -23,7 +23,7 @@ module Babl
                 protected
 
                 # Override TemplateBase#precompile to ensure that all pin dependencies are satisfied.
-                def precompile
+                def precompile(*)
                     super.tap do |node|
                         raise Errors::InvalidTemplate, 'Unresolved pin' unless node.pinned_dependencies.empty?
                     end

spec/babl_spec.rb
@@ -1,0 +1,25 @@
+# frozen_string_literal: true
+require 'spec_helper'
+
+describe Babl do
+    subject { compiled.render(nil) }
+
+    describe '#compile' do
+        context 'block' do
+            let(:compiled) { Babl.compile { static(1) } }
+            it { is_expected.to eq 1 }
+        end
+
+        context 'template' do
+            let(:compiled) { Babl.compile(Babl.template.static(1)) }
+            it { is_expected.to eq 1 }
+        end
+    end
+
+    describe Babl::AbsoluteLookupContext do
+        let(:lookup_context) { described_class.new(File.dirname(__FILE__)) }
+        let(:compiled) { Babl.source { partial('test_template') }.compile(lookup_context: lookup_context) }
+
+        it { is_expected.to eq(this_is_a: 'test') }
+    end
+end

commit 20141b0207c6cc8dc113547e0b02278d338f19c1

Fix regression introduced by 173c7b556313d0d7a66dd91f6e28b0b77d0947b9 (#81)

lib/babl/operators/partial.rb
@@ -8,7 +8,7 @@ module Babl
                 # Load a partial template given its name
                 # A 'lookup_context' must be defined
                 def partial(partial_name)
-                    current_template = self
+                    current_template = unscoped
                     construct_terminal { |ctx|
                         lookup_context = ctx[:lookup_context]
                         raise Errors::InvalidTemplate, 'Cannot use partial without lookup context' unless lookup_context

spec/operators/partial_spec.rb
@@ -15,7 +15,7 @@ describe Babl::Operators::Partial do
     let(:lookup_context) {
         TestLookupContext.new(
             blabla: TestLookupContext.new(
-                "[partial('navi'), partial('muche')]",
+                "[{x: _.partial('navi')}, partial('muche')]",
                 muche: TestLookupContext.new('23'),
                 navi: TestLookupContext.new(
                     "using(MyExt) { partial('navi') }",
@@ -31,7 +31,7 @@ describe Babl::Operators::Partial do
         )
     }
 
-    let(:object) { 12 }
+    let(:object) { { x: 12 } }
 
     context 'missing partial' do
         template { partial('i_do_not_exist') }
@@ -42,6 +42,6 @@ describe Babl::Operators::Partial do
     context 'found partial' do
         template { partial('blabla') }
 
-        it { expect(json).to eq [['hello', 13], 23] }
+        it { expect(json).to eq [{ 'x' => ['hello', 13] }, 23] }
     end
 end